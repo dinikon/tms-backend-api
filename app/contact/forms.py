656d4d093044b9df50ea94da13b3d5e952bff4b6
inikon <denis.inikon@gmail.com>
2024-05-15 16:17:43 +0300

    Add jquery to address forms locations trunc and contacts.

--- app/contact/forms.py
+++ app/contact/forms.py
@@ -1,13 +1,28 @@
 from django import forms
-from django_select2.forms import Select2Widget
+from django_select2.forms import Select2Widget, ModelSelect2Widget
 from .models import CRMContact, CRMCompanies
 from truck.models import PostalCodeInfo
 
 
+class PostalCodeSelect2Widget(ModelSelect2Widget):
+    search_fields = [
+        'place_name__icontains',
+        'postal_code__icontains',
+        'country_code__icontains'
+    ]
+
+    model = PostalCodeInfo
+    queryset = PostalCodeInfo.objects.all()
+
+    def label_from_instance(self, obj):
+        return f"{obj.place_name}, {obj.postal_code}, {obj.country_code}"
+
+
 class CRMContactForm(forms.ModelForm):
-    address = forms.ChoiceField(
-        widget=Select2Widget(),
-        required=False,  # Указываем, что поле необязательное
+    address = forms.ModelChoiceField(
+        queryset=PostalCodeInfo.objects.none(),
+        widget=PostalCodeSelect2Widget,
+        required=False,
         label="Address",
         initial='',  # Устанавливаем начальное значение на пустое
     )
@@ -15,16 +30,6 @@ class CRMContactForm(forms.ModelForm):
     class Meta:
         model = CRMContact
         fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super(CRMContactForm, self).__init__(*args, **kwargs)
-        # Сначала добавляем пустой выбор
-        empty_choice = [('', '--- Select ---')]
-        postal_choices = [
-            (postal.id, f"{postal.place_name}, {postal.postal_code}, {postal.country_code}")
-            for postal in PostalCodeInfo.objects.all()
-        ]
-        self.fields['address'].choices = empty_choice + postal_choices
 
 
 class CRMCompanyForm(forms.ModelForm):
